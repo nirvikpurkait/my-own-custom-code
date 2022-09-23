1ab141d12ab097e3a794abd4f8c43dd24cdf27bd
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-09-23 20:45:52 +0530

    Update script.js
    for syncing with animation

--- typing text/script.js
+++ typing text/script.js
@@ -1,18 +1,22 @@
 // put all the text want to show them one by one in side the array
+
 const textToShow =
   [
-    'Lorem ipsum dolor sit,',
-    'amet consectetur adipisicing elit.',
-    'Voluptatum, odio',
-    'soluta expedita cum repellendus praesentium!'
+    'fron end developer',
+    'back end developer',
+    'full-stack developer',
+    'UI/UX developer'
   ]
-
 const secondText = document.querySelector('.second-text')
 
 secondText.textContent = textToShow[0]
+secondText.style.animation = `text 10000ms linear infinite`
 secondText.style.animationTimingFunction = `steps(${textToShow[0].length})`
 
+
+
 setInterval(showText, 10000);
+
 
 function showText() {
   let currentText = secondText.textContent
